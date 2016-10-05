cfe6ef4f596aa93382d844b5a7f11939b579d6bb
Cristian Ramírez <cristianramirezgt@gmail.com>
2016-10-04 23:26:29 -0600

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cristian Ramírez
+ * Date: 4/10/2016
+ * Time: 10:06 PM
+ */
+
+// Firebase API key
+define('FIREBASE_API_KEY', 'AIzaSyBKMY9Vbxc_sZv8YQun-b8rcO7KcRI6FGs');
